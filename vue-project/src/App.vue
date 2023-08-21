89217069c3bd69f9d2e7eca4173a74df37a8528a
zhaozhiliao <zhaodong199208@gmail.com>
2023-08-21 15:14:50 +0800

    main

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>模版语法</h3>
+  <p>{{ msg }}</p>
+  <p>{{ hello }}</p>
+  <p>{{ number + 1 }}</p>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "神奇的魔法",
+      hello: "hello world",
+      number: 10,
+    };
+  },
+};
+</script>
